8ccd25b5194d6158e52d35186b1d82052c3c7269
sariya <dayzwaitik123@yandex.ru>
2024-03-09 23:56:59 +0300

    Parse salary

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import requests
 
-def get_vacancies(keyword):
+def get_vacancies(keyword: str, programming_language: str="Python") -> None:
     url = "https://api.hh.ru/vacancies"
     params = {
         "text": keyword,
-        "area": 1,  # Specify the desired area ID (1 is Moscow)
-        "per_page": 100,  # Number of vacancies per page
-        "experience": "Нет опыта",
+        "area": 113,
+        "per_page": 100, 
+        "experience": "noExperience",
+        "currency": "RUR",
+        "date_from": "2024-03-01",
     }
     headers = {
-        "User-Agent": "Your User Agent",  # Replace with your User-Agent header
+        "User-Agent": "Your User Agent",
     }
 
     response = requests.get(url, params=params, headers=headers)
-
+    result = []
     if response.status_code == 200:
         data = response.json()
         vacancies = data.get("items", [])
+        print(type(vacancies))
         for vacancy in vacancies:
-            # Extract relevant information from the vacancy object
+            vacancy: dict = vacancy
             vacancy_id = vacancy.get("id")
             vacancy_title = vacancy.get("name")
             vacancy_url = vacancy.get("alternate_url")
             company_name = vacancy.get("employer", {}).get("name")
-            print(f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\n")
+            salary = vacancy["salary"]
+            # print(vacancy["salary"])
+            # print(salary)
+            result.append(
+                [vacancy_id, vacancy_title, vacancy_url, company_name, salary, programming_language]
+            )
+            print(f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\nSalary: {salary}\n\n")
+        return result
     else:
         print(f"Request failed with status code: {response.status_code}")
 
 
 # Example usage
 get_vacancies("junior python")
+d = {
+    "1": "a"
+}
+print(d.get("2", 0))
